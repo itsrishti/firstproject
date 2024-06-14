f550f53cc0f3028801367158c108f577eebeda99
itsrishti <123953052+itsrishti@users.noreply.github.com>
2024-06-14 18:35:30 +0530

    firstproject

---
+++ src/component/Basics/Reataurant.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "./Style.css";
+const Reataurant = () => {
+  return (
+    <>
+<div className='card-container'>
+    <div className='card'>
+        <div className='card-body'>
+            <span className='card-number card-circle subtle'>1</span>
+        </div>
+
+    </div>
+</div>
+
+    </>
+  )
+}
+
+export default Reataurant
